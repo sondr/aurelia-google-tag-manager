65ad321c517c23bd7396588f198dbbcc79f8159f
Sondre <sondre.erno@gmail.com>
2018-06-20 08:53:43 +0200

    bug fix in scriptElement text

--- dist/es2015/tag-manager.js
+++ dist/es2015/tag-manager.js
@@ -46,7 +46,7 @@ var TagManager = /** @class */ (function () {
     };
     TagManager.prototype._attachScriptElements = function (key) {
         var scriptElement = DOM.createElement('script');
-        scriptElement.text = "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});varf=d.getElementsByTagName(s)[0]," +
+        scriptElement.text = "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0]," +
             "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);" +
             ("})(window,document,'script','dataLayer','" + key + "');");
         var noscriptElement = DOM.createElement('noscript');
